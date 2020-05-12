3d12eb9808ed88cd75c23997aff4f8e3d5104bb1
shaun <ycorredius@gmail.com>
2020-05-12 12:23:04 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App'
+import { createStore,applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import catsReducer from './reducers/catsReducer'
+import { Provider } from 'react-redux';
 
+const store = createStore(catsReducer,
+    applyMiddleware(thunk))
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
